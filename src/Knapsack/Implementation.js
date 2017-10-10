f44201ae7ea4e5d269d422447092b3397e78af5f
tkonz <Tish@Tishas-MacBook-Air.local>
2017-10-10 16:08:20 -0600

    added knapsack algorithm implementation

--- src/Knapsack/Implementation.js
+++ src/Knapsack/Implementation.js
@@ -6,15 +6,49 @@ class Implementation{
     return 1;
   }
 
-  nextIteration(tableData) {
 
-    let row = tableData[0];
 
-    console.log(row);
-    row[1] = row[1] + 1;
-    return tableData
-  }
+nextIteration(arr)
+{
+  //var K = new Array(n+1); // n items
+  //for (var i=0; i <= n; i++)
+  //{
+    //K[i] = new Array(W+1);
+    var W = 3;
+    var wt=[];
+    var val=[];
+    var n = 2;
 
+    var i=0;
+
+    for (var w=0; w<=W; w++)
+    {
+      // Base case
+      if (i == 0 || w == 0)
+        arr[i][w] = 0;
+
+      else if (wt[i-1] <= w) 
+        arr[i][w] = max(val[i-1] + arr[i-1][w-wt[i-1]], arr[i-1][w]);
+      
+      else
+        arr[i][w] = arr[i-1][w];
+    }
+    //**HERE is where we can return the entire column before moving to the next row
+    
+  //}
+  // This returns the solution in value
+  return arr;
+
+}
+
+
+
+
+}
+
+function max(a, b)
+{
+  return ((a > b) ? a : b);
 }
 
 export default Implementation
